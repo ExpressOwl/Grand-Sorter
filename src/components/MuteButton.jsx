5f6fbc8558b49471d3544a9d28a5f3cdadc2e1a1
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-06-01 05:59:59 +0100

    Favicon Added

--- src/components/MuteButton.jsx
+++ src/components/MuteButton.jsx
@@ -6,7 +6,7 @@ const Mute = ({ isMuted, onToggleMute }) => {
       className="text-md mx-4 mb-10 flex h-[90px] w-[90px] items-center justify-center bg-[url(./assets/button.gif)] bg-contain bg-center bg-no-repeat text-center text-[#90c040]"
       onClick={onToggleMute}
     >
-      {isMuted ? "Unmute" : "Mute"}
+      {isMuted ? "Unmute" : "Mute SFX"}
     </button>
   );
 };
